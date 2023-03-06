2854dac364ccabe03dc91a2f8ddaa9f42439572e
alejandro <aneachi128@g.educaand.es>
2023-03-06 14:42:54 +0100

    ej 12 empezado

---
+++ src/main/java/ej12/Cuenta.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ej12;
+
+/**
+ *
+ * @author alejandro
+ */
+public abstract class Cuenta {
+    private String numeroCuenta;
+    private double saldo;
+    private Persona cliente;
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public Persona getCliente() {
+        return cliente;
+    }
+    
+    
+}
---
+++ src/main/java/ej12/Persona.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ej12;
+
+/**
+ *
+ * @author alejandro
+ */
+public class Persona {
+    private String nombre;
+    private String apellidos;
+    private String nif;
+
+    public Persona(String nombre, String apellidos, String nif) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.nif = nif;
+    }
+
+    public Persona() {
+    }    
+}
